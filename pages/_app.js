97fd1d02555262ec2b449ebbdc1fc862250f59c0
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2023-10-30 10:06:12 +0100

    Feat: Cart will be persist now if page is reloaded

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,20 @@ import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect, useState } from "react";
-import { Provider } from "react-redux";
-import Toaster from "../src/components/Helpers/Toaster";
-import DefaultLayout from "../src/components/Partials/DefaultLayout";
-import store from "../src/store/store";
-import "../styles/globals.css";
-import "../styles/loader.css";
-import "../styles/selectbox.css";
 import Router from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
-import MaintenanceWrapper from "../src/components/Partials/Headers/MaintenanceWrapper";
+import { useEffect, useState } from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import LoginContext from "../src/components/Contexts/LoginContexts";
+import Toaster from "../src/components/Helpers/Toaster";
+import DefaultLayout from "../src/components/Partials/DefaultLayout";
+import MaintenanceWrapper from "../src/components/Partials/Headers/MaintenanceWrapper";
+import { persistor, store } from "../src/store/store";
+import "../styles/globals.css";
+import "../styles/loader.css";
+import "../styles/selectbox.css";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -33,15 +34,17 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Provider store={store}>
-        <LoginContext.Provider
-          value={{ loginPopup: loginPopup, handlerPopup: handlerPopup }}
-        >
-          <DefaultLayout>
-            <MaintenanceWrapper>
-              <Component {...pageProps} />
-            </MaintenanceWrapper>
-          </DefaultLayout>
-        </LoginContext.Provider>
+        <PersistGate loading={null} persistor={persistor}>
+          <LoginContext.Provider
+            value={{ loginPopup: loginPopup, handlerPopup: handlerPopup }}
+          >
+            <DefaultLayout>
+              <MaintenanceWrapper>
+                <Component {...pageProps} />
+              </MaintenanceWrapper>
+            </DefaultLayout>
+          </LoginContext.Provider>
+        </PersistGate>
       </Provider>
       <Toaster />
     </>
--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,41 @@
 import { configureStore } from "@reduxjs/toolkit";
-import websiteSetup from "./websiteSetup";
-import wishlistData from "./wishlistData";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import cart from "./Cart";
 import compareProduct from "./compareProduct";
+import websiteSetup from "./websiteSetup";
+import wishlistData from "./wishlistData";
 
-export default configureStore({
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cart);
+
+export const store = configureStore({
   reducer: {
     websiteSetup: websiteSetup,
     wishlistData: wishlistData,
-    cart: cart,
+    cart: persistedReducer,
     compareProducts: compareProduct,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
